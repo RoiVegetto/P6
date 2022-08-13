13503a56a75ac95c28c42e9f100ed129f130a6dd
Benjamin Corroyez <benjamincorroyez@hotmail.fr>
2022-08-13 15:29:42 +0200

    création des likes et dislikes

--- back/controllers/like.js
+++ back/controllers/like.js
@@ -4,8 +4,50 @@ exports.likeFicheUser = (req, res, next) => {
   User
   .findOne({_id : req.params.id})
   .then((objet) => {
-    if(!objet.usersLiked.includes(req.body.userId) && req.body.like === 1)
-    res.status(200).json(objet);
-  })
-  .catch((error) => res.status(404).json({error}));
+    if(!objet.usersLiked.includes(req.body.userId) && req.body.like === 1){
+// Mise a jour de l'objet BDD
+    User.updateOne(
+      {_id : req.params.id},
+      {
+        $inc: {likes: 1},
+        $push: {usersLiked: req.body.userId}
+      }
+  )
+
+.then(() => res.status(201).json({message: "User like +1"}))
+.catch((error) => res.status(400).json({error}));
+};
+
+  if(!objet.usersLiked.includes(req.body.userId) && req.body.like === 1){
+// Mise a jour de l'objet BDD
+  User.updateOne(
+    {_id : req.params.id},
+    {
+      $inc: {likes: -1},
+      $pull: {usersLiked: req.body.userId}
+    }
+  )
+
+.then(() => res.status(201).json({message: "User like +1"}))
+.catch((error) => res.status(400).json({error}));
+
+};
+
+if(!objet.usersDisLiked.includes(req.body.userId) && req.body.like === -1){
+  // Mise a jour de l'objet BDD
+      User.updateOne(
+        {_id : req.params.id},
+        {
+          $inc: {dislikes: 1},
+          $push: {usersDisLiked: req.body.userId}
+        }
+    )
+  
+  .then(() => res.status(201).json({message: "User dislike +1"}))
+  .catch((error) => res.status(400).json({error}));
+  };
+
+})
+.catch((error) => res.status(404).json({error}));
+
 }
